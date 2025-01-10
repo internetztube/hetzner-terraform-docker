3ef0ec8650543f072469e423cebaf3a265cd9e71
Frederic Koeberl <hello@frederickoeberl.com>
2025-01-10 11:46:46 +0100

    Update server.tf example.

--- example/server.tf
+++ example/server.tf
@@ -1,17 +1,16 @@
 module "main" {
-  source       = "github.com/internetztube/hetzner-terraform-docker"
-  name         = "main"
-  server_type  = "cx22"
-  location     = var.location
-  floating_ip  = hcloud_floating_ip.main
-  volume       = hcloud_volume.main
-  firewall_ids = [hcloud_firewall.http.id]
-
+  source                   = "github.com/internetztube/hetzner-terraform-docker"
+  name                     = "main"
+  server_type              = "cx22"
+  location                 = var.location
+  floating_ip              = hcloud_floating_ip.main
+  volume                   = hcloud_volume.main
+  firewall_ids             = [hcloud_firewall.http.id]
+  create_final_snapshot    = true
   containers_folder        = abspath("./containers")
   docker_compose_file_path = abspath("./docker-compose.yml")
-
-  ssh_key_id      = hcloud_ssh_key.default.id
-  ssh_private_key = var.ssh_private_key
+  ssh_key_id               = hcloud_ssh_key.default.id
+  ssh_private_key          = var.ssh_private_key
 }
 
 resource "hcloud_firewall" "http" {
