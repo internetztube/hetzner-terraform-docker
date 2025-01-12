a132bf51464257ebe7a5a18c1773572c1f7a090e
Frederic Koeberl <hello@frederickoeberl.com>
2025-01-13 00:20:32 +0100

    Create volume folders for all services defined in docker-compose.yml.

--- scripts/docker-load.sh
+++ scripts/docker-load.sh
@@ -8,8 +8,12 @@ for tar_file in /root/container-artifacts/*.tar; do
   if [ -f "${tar_file}" ]; then
     echo "Loading Docker image from: ${tar_file}"
     docker load -i "${tar_file}"
-    CONTAINER_NAME="$(basename "${tar_file}" .tar)"
-    mkdir -p "/mnt/volume/${CONTAINER_NAME}"
   fi
 done
+
+services="$(yq eval '.services | keys[]' /root/docker-compose.yml)"
+for service_name in $services; do
+  mkdir -p "/mnt/volume/${service_name}"
+done
+
 systemctl restart docker-compose
