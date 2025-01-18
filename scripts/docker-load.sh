b6966a8f45e8c832141a007bd3a133bc9d29ec83
Frederic Koeberl <hello@frederickoeberl.com>
2025-01-18 22:25:34 +0100

    docker compose pull

--- scripts/docker-load.sh
+++ scripts/docker-load.sh
@@ -11,10 +11,17 @@ for tar_file in /root/container-artifacts/*.tar; do
   fi
 done
 
+# Each container from docker-compose.yml gets their own persistent folder.
 services=$(yq -r '.services | keys[]' /root/docker-compose.yml)
 for service in ${services}; do
   mkdir -p "/root/volume/${service}"
 done
 
+# Pull latest container images.
+docker compose pull --ignore-pull-failures
+
+# Restart!
 systemctl restart docker-compose
+
+# Delete old container images.
 docker image prune -f
